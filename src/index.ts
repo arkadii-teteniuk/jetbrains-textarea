8e84b515165c66f8ccd3a351e7fc848677731ea5
Arkadii Teteniuk <arkadiy.tetenyuk@quantumsoft.ru>
2023-07-16 20:45:00 +0200

    Remove text repeating

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { initSearch } from "./initSearch";
 import { INITIAL_SEARCH_TEXT, INITIAL_SEARCH_VALUE } from "./constants";
 
 window.addEventListener("load", () => {
-  initSearch(INITIAL_SEARCH_TEXT.repeat(25), INITIAL_SEARCH_VALUE);
+  initSearch(INITIAL_SEARCH_TEXT, INITIAL_SEARCH_VALUE);
 });
